983efc0b9b3af7e47af8e2f943c6fe4dc2198e70
mrkrca <aleksa.djuric.dj@gmail.com>
2025-01-11 00:05:48 +0100

    UPDATED

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://sqlmoviesite-production.up.railway.app/auth/google/callback"
+  callbackURL: "https://sqlmoviesite-production.up.railway.app/auth/google/callback",
 
 },
 async function(accessToken, refreshToken, profile, done) {
